fc3d18a380ba500d8939ebf3e4b7eb0399d6a8a0
Vallabh0440 <157093445+Vallabh0440@users.noreply.github.com>
2024-07-21 17:25:22 +0530

    Add files via upload

---
+++ arithmatic.c
@@ -0,0 +1,21 @@
+
+#include<stdio.h>
+void main(){
+
+int a;
+float b;
+double c;
+char f;
+char e[]="";
+scanf("%d",&a);
+printf("Hello This is Our First Practical %d\n",a);
+scanf("%f",&b);
+printf("Hello This is Our First Practical of float %f\n",b);
+scanf("%lf",&c);
+printf("Hello This is Our First Practical of double %lf\n",c);
+scanf("%s",&e);
+printf("Hello This is Our First Practical of char %s\n",e);
+scanf("%s",&f);
+printf("Hello This is Our First Practical of char %c",f);
+
+}
