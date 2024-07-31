1dc552b3fb288ab08d5a4f3cf62f7423ec0b097f
Vallabh0440 <157093445+Vallabh0440@users.noreply.github.com>
2024-07-31 10:13:47 +0530

    Add files via upload

---
+++ Assignment.c
@@ -0,0 +1,35 @@
+#include<stdio.h>
+int main(){
+    int a = 50;
+    // a += 10;
+
+    // printf("value of + assigment operator is %d\n",a);
+
+    // a -= 10;
+    // printf("value of - assigment operator is %d\n",a);
+
+    // a *= 10;
+    // printf("value of * assigment operator is %d\n",a);
+
+    // a /= 10;
+    // printf("value of / assigment operator is %d\n",a);
+
+    // // a %= 20;
+    // // printf("value of % assigment operator is %d\n",a);
+
+    // a &= 10;
+    // printf("value of & assigment operator is %d\n",a);
+
+    // a |= 10;
+    // printf("value of | assigment operator is %d\n",a);
+
+    // a ^= 10;
+    // printf("value of ^ assigment operator is %d\n",a);
+
+    a <<= 5;
+    printf("value of << assigment operator is %d\n",a);
+
+    a >>= 3;
+    printf("value of >> assigment operator is %d\n",a);
+
+}
